9fc5cceb0f3761e43c21836a780ab6ad06e2537d
jlkesh <john.lgd65@gmail.com>
2022-03-13 10:19:11 +0500

    intro

--- src/main/java/uz/jl/springdata/db/Init.java
+++ src/main/java/uz/jl/springdata/db/Init.java
@@ -1,11 +1,13 @@
 package uz.jl.springdata.db;
 
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 import uz.jl.springdata.domain.Book;
 import uz.jl.springdata.repository.BookRepository;
 
 @Component
+@Profile({"dev", "tes"})
 public record Init(BookRepository bookRepository) implements CommandLineRunner {
 
     @Override
